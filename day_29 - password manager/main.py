28bc9575a0d120f1bd7456db6935af4ca3891e12
davidkowalik <dawid7c@gmail.com>
2023-12-20 23:23:09 +0100

    day - 30

--- day_29 - password manager/main.py
+++ day_29 - password manager/main.py
@@ -2,6 +2,28 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search():
+    website = webside_ent.get()
+
+    if len(website) == 0:
+        messagebox.showinfo(title="Something is wrong?", message="Check webside You trying to search")
+    else:
+        try:
+            with open("data.json", "r") as json_data_file:
+                # read json data
+                data = json.load(json_data_file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Something is wrong?", message="JSON file missing, check this")
+        else:
+            if website in data:
+                messagebox.showinfo(title=website, message=f"Email: {data[website]['email']}\nPassword: {data[website]['password']}")
+            else:
+                messagebox.showinfo(title="Something is wrong?", message=f"You do not have password for {website}")
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -24,27 +46,41 @@ def generate_password():
     pass_ent.insert(END, password)
     
 
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def add_password():
+def save():
     website =webside_ent.get()
     email = email_ent.get()
     password = pass_ent.get()
+    new_data = {
+        website : {
+            "email" : email,
+            "password" : password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Something is wrong?", message="You left some box(es) empty, please check.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n\nEmail: {email} \nPassword: {password} \n\nIs it ok to save?")
-        
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+        try:
+            with open("data.json", "r") as json_data_file:
+                # read json data
+                data = json.load(json_data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as json_data_file:
+                # save updated json data
+                json.dump(new_data, json_data_file, indent=4)
+        else:
+            # update json data
+            data.update(new_data)
 
+            with open("data.json", "w") as json_data_file:
+                # save updated json data
+                json.dump(data, json_data_file, indent=4)
+        finally:    
             webside_ent.delete(0, END)
             pass_ent.delete(0, END)
             webside_ent.focus()
-        else:
-            pass
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # widow setup
@@ -70,15 +106,18 @@ pass_lb = Label( text="Password:")
 pass_lb.grid(column=0, row=3)
 
 # buttons setup
-add = Button(text="Add", width=58, command=add_password)
+add = Button(text="Add", width=58, command=save)
 add.grid(column=1, row=4, columnspan=2)
 
-gen_pass =  Button(text="Generate Password", command=generate_password)
+gen_pass =  Button(text="Generate Password", width=15, command=generate_password)
 gen_pass.grid(column=2, row=3)
 
+search =  Button(text="Search", width=15, command=search)
+search.grid(column=2, row=1, columnspan=1)
+
 # entry setup
-webside_ent = Entry(width=46, font=("consolas"))
-webside_ent.grid(column=1, row=1, columnspan=2)
+webside_ent = Entry(width=33, font=("consolas"))
+webside_ent.grid(column=1, row=1)
 webside_ent.focus() # cursor ready in first entry after starting program
 
 email_ent = Entry(width=46, font=("consolas"))
