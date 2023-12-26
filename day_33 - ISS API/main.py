8037f4cc8389a9eb4799210a584a79950f8a16fc
davidkowalik <dawid7c@gmail.com>
2023-12-26 18:28:19 +0100

    day-32

---
+++ day_33 - ISS API/main.py
@@ -0,0 +1,55 @@
+import requests
+from datetime import datetime
+import time
+
+MY_LAT = 50.033112
+MY_LNG = 20.217400
+
+def is_iss_over():
+    response = requests.get(url='http://api.open-notify.org/iss-now.json')
+    response.raise_for_status()
+
+    data = response.json()
+    iss_lng = float(data['iss_position']['longitude'])
+    iss_lat = float(data['iss_position']['latitude'])
+    iss_position = (iss_lat, iss_lng)
+
+    print(iss_position)
+
+    if abs(MY_LAT - iss_lat) < 5 and abs(MY_LNG - iss_lng) < 5:
+        return True
+    else:
+        return False
+
+def is_night():
+    parameters = {
+        "lat":MY_LAT,
+        "lng":MY_LNG,
+        "formatted":0,
+    }
+
+    niep_response = requests.get(url='https://api.sunrise-sunset.org/json', params=parameters)
+    niep_response.raise_for_status()
+
+    data = niep_response.json()
+    sunrise_h = int(data['results']['sunrise'].split('T')[1].split(':')[0])
+    sunset_h = int(data['results']['sunset'].split('T')[1].split(':')[0])
+
+    print(f"Sunrise: {sunrise_h}")
+    print(f"Sunset: {sunset_h}")
+    hour_now = datetime.utcnow().hour
+    print(hour_now)
+    if sunrise_h > hour_now or sunset_h < hour_now:
+        return True
+    else:
+        return False
+
+print(is_iss_over())
+print(is_night())
+
+while True:
+
+    if is_iss_over() and is_night():
+        print("Look up, ISS over You.")
+
+    time.sleep(60)
