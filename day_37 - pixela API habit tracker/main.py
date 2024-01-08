837fec592f2a446800d93dd3453803be4f626435
davidkowalik <dawid7c@gmail.com>
2024-01-08 18:03:05 +0100

    day-37

--- day_37 - pixela API habit tracker/main.py
+++ day_37 - pixela API habit tracker/main.py
@@ -1,4 +1,7 @@
 import requests
+from datetime import datetime
+
+# my graph url: https://pixe.la/v1/users/noobby/graphs/graph001.html
 
 PIXELA_TOKEN = "d$3k*Ah&F2JTAjWM"
 USER_NAME = "noobby"
@@ -11,21 +14,51 @@ users_params = {
     "agreeTermsOfService":"yes",
     "notMinor":"yes"
 }
-# # create new user
+
+pixela_headers = {
+    "X-USER-TOKEN":PIXELA_TOKEN
+}
+
+# # create new user-----------------------------------------------
 # r = requests.post(url=pixela_endpoint, json=users_params)
 # print(r.text)
 
-# create new graph
+# # create new graph------------------------------------------
 
-graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
-print(graph_endpoint)
+# graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
+# print(graph_endpoint)
 
-graph_params = {
-    "id":GRAPH_ID,
-    "name":"Programing study",
-    "unit":"Hour",
-    "type":"float",
-    "color":"ajisai",
+
+# graph_params = {
+#     "id":GRAPH_ID,
+#     "name":"Programing study",
+#     "unit":"Hour",
+#     "type":"float",
+#     "color":"ajisai",
+# }
+# r = requests.post(url=graph_endpoint, json=graph_params, headers=pixela_headers)
+# print(r.text)
+
+# add pixel to the graph------------------------------------------
+today = datetime.now()
+
+s=today.strftime("%Y%m%d")
+
+add_pixel_body={
+    "date":today.strftime("%Y%m%d"),
+    "quantity":"2",
 }
-r = requests.post(url=graph_endpoint, json=graph_params)
-prnit(r.text)
+
+add_pixel_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPH_ID}"
+r = requests.post(url=add_pixel_endpoint, json=add_pixel_body, headers=pixela_headers)
+print(r.text)
+print(s)
+
+# update pixel-------------------------------------------
+# update_pixel_body={
+#     "quantity":"1",
+# }
+
+# update_pixel_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+# r = requests.delete(url=update_pixel_endpoint, headers=pixela_headers)
+# print(r.text)
